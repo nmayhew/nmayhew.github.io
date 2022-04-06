22b83955d8ebc291f7628ccb13bdff0aa230db43
Nick Mayhew <nmayhew@stanford.edu>
2022-04-06 12:17:30 -0700

    Fonts and Border Radius
    Changing Feel of website

--- src/pages/intro/intro.js
+++ src/pages/intro/intro.js
@@ -11,7 +11,7 @@ function Intro() {
     <div id="intro" className="intro">
           <div id="intro" className="intro-text">
            <h1>Hi, I'm Nick Mayhew</h1>
-           <br></br><br></br>
+           <br></br>
            <p>Born and raised in London, I'm graduating Stanford University as Captain of the men’s varsity rowing team with a 4.0 GPA in Computer Science in June. 
            <br></br><br></br>
             From October, I'll be studying a Master's in management at Cambridge University after completing a Software Engineering Internship at Amazon this summer.
