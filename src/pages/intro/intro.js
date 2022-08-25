c453ce92344e08fd50676369c4e5d59ee72c309e
Nick Mayhew <nmayhew@stanford.edu>
2022-08-25 15:09:16 +0100

    Highlighting keywords

--- src/pages/intro/intro.js
+++ src/pages/intro/intro.js
@@ -18,9 +18,9 @@ function Intro() {
           <div id="intro" className="intro-text">
            <h1>Hi, I'm Nick Mayhew</h1>
            <br></br>
-           <p>I graduated Stanford University with distinction in June in Computer Science (4.05 GPA), winning the Block S Honor for highest GPA of a male graduating Student-Athlete and captaining Men's Varsity Rowing. 
+           <p>I graduated <mark>&nbsp;Stanford&nbsp;</mark> University studying <mark>&nbsp;CS&nbsp;</mark> with distinction (4.05 GPA), winning the Block S Honor for highest GPA of a male graduating Student-Athlete and captaining Men's Varsity Rowing. 
            <br></br><br></br>
-            I'm currently studying a Master's in Management at Cambridge University after completing a Software Engineering Internship at Amazon this summer.
+            I'm currently studying a Master's in <mark>&nbsp;Management&nbsp;</mark> at <mark>&nbsp;Cambridge&nbsp;</mark> University after a <mark>&nbsp;Software Engineering&nbsp;</mark> Internship at <mark>&nbsp;Amazon&nbsp;</mark>.
             <br></br><br></br>
             I'm always looking for new opportunities. Reach out to me on Linkedin or check out my GitHub!</p>
             <br></br>
