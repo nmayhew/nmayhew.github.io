5d3de0374a6997fd5085ac17d2af3fdcdf271cbb
nmayhew <66178080+nmayhew@users.noreply.github.com>
2023-08-03 09:11:12 +0100

    remove animations

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Work from "./pages/work/work";
 import Project from "./pages/projects/projects";
 import Programming from "./pages/programming/programming";
 import ReactGA from 'react-ga';
-import { ScrollContainer } from "react-scroll-motion";
+
 function App() {
   ReactGA.initialize('UA-232337955-1');
   ReactGA.pageview(window.location.pathname + window.location.search);
@@ -29,13 +29,11 @@ function App() {
   return (
     <div className="app">
       <div className="content">
-        <ScrollContainer >
-          <Intro />
-          <Education />
-          <Work />
-          <Project />
-          <Programming />
-        </ScrollContainer>
+        <Intro />
+        <Education />
+        <Work />
+        <Project />
+        <Programming />
       </div>
       <Navbar />
 
--- src/pages/programming/programming.jsx
+++ src/pages/programming/programming.jsx
@@ -1,41 +1,38 @@
 import "./programming.css";
 import rowingPhoto from '../../assets/backdrop/backdrop.JPG';
 import MaterialIcon from 'material-icons-react';
-import { Animator, ScrollPage, batch, FadeIn, MoveIn } from "react-scroll-motion";
 import { useMediaQuery } from 'react-responsive';
 import CV from '../../assets/cv/CVWebsitePublished.pdf'
 
 function Programming() {
   const isMobile = useMediaQuery({ query: '(max-width: 450px)' })
   return (
-    <ScrollPage>
-      <div id="programming" className="programming">
-        <div className="programming-overHead">
-          {isMobile && <MaterialIcon icon="code" size={40} color='#FFFFFF' />}
-          {!isMobile && <MaterialIcon icon="code" size={55} color='#FFFFFF' />}
-          <h1 >
-            <a className="programming-header"
-              href="https://docs.google.com/document/d/1NCGYl5js3ivfhzCUfKkWKveYvtpqMtz-RgT6pUw0JGo/edit?usp=sharing">
-              Resume
-            </a>
-          </h1>
-        </div>
-        <div className="programming-content">
-          <embed src={CV}
-            type="application/pdf"
-            className="programming-progress"
-            marginWidth="0"
-            marginHeight="0">
 
-          </embed>
-
-          <Animator className="programming-photo" animation={batch(FadeIn(), MoveIn(400, 0, 0, 0))}>
-            <img src={rowingPhoto} alt="Racing at Stanford" className="programming-photo" />
-          </Animator>
-        </div>
-
+    <div id="programming" className="programming">
+      <div className="programming-overHead">
+        {isMobile && <MaterialIcon icon="code" size={40} color='#FFFFFF' />}
+        {!isMobile && <MaterialIcon icon="code" size={55} color='#FFFFFF' />}
+        <h1 >
+          <a className="programming-header"
+            href="https://docs.google.com/document/d/1NCGYl5js3ivfhzCUfKkWKveYvtpqMtz-RgT6pUw0JGo/edit?usp=sharing">
+            Resume
+          </a>
+        </h1>
       </div>
-    </ScrollPage>
+      <div className="programming-content">
+        <embed src={CV}
+          type="application/pdf"
+          className="programming-progress"
+          marginWidth="0"
+          marginHeight="0">
+
+        </embed>
+
+        <img src={rowingPhoto} alt="Racing at Stanford" className="programming-photo" />
+      </div>
+
+    </div >
+
   );
 }
 
